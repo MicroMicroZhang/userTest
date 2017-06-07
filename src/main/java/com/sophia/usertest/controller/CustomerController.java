ad654bf364e71633fb20af0cdd2d08a6baf556b0
zww <11740393@qq.com>
2017-06-07 16:46:48 +0800

    first commit

--- src/main/java/com/sophia/usertest/controller/CustomerController.java
+++ src/main/java/com/sophia/usertest/controller/CustomerController.java
@@ -1,11 +1,6 @@
 package com.sophia.usertest.controller;
-import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,16 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sophia.usertest.service.CustomerService;
-import com.sophia.usertest.service.UserService;
 import com.sophia.usertest.vo.Customer;
-import com.sophia.usertest.vo.User;
-import com.sophia.util.CreateXL;
 
 @Controller
 public class CustomerController {
